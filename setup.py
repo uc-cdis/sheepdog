4c3ab3c34b99d9f7c41788b9ed83295f288e7894
Jimmy <OpenProfOak@gmail.com>
2017-12-19 13:52:23 -0600

    feat(api-373): consolidate to setup.py
    - Move everything into setup.py
    - Fix dependency conflicts
    - added .gitignore file
    - changed travis yaml to use setup.py
    - pin to specific version of postgres
    - bump nci-gdc/gdcdictionary pin
        - psqlgraph pulled in the the old gdcdatamodel hash is mismatching the one
        specified in the setup.py file

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 
-
 setup(
     name='sheepdog',
     version='0.1.0',
@@ -23,5 +22,37 @@ setup(
         'sheepdog.transactions.upload',
         'sheepdog.utils',
         'sheepdog.utils.transforms',
+    ],
+    install_requires=[
+        'boto==2.36.0',
+        'cryptography==2.0.3',
+        'Flask-Cors==1.9.0',
+        'Flask-SQLAlchemy-Session==1.1',
+        'Flask==0.10.1',
+        'fuzzywuzzy==0.6.1',
+        'graphene==0.10.2',
+        'jsonschema==2.5.1',
+        'lxml==3.8.0',
+        'psycopg2==2.7.3.2',
+        'PyYAML==3.11',
+        'requests==2.5.2',
+        'setuptools==30.1.0',
+        'simplejson==3.8.1',
+        'sqlalchemy==0.9.9',
+        'psqlgraph',
+        'userdatamodel',
+        'cdisutils',
+        'cdis_oauth2client',
+        'gdcdictionary',
+        'gdcdatamodel',
+    ],
+    dependency_links=[
+        'git+https://git@github.com/NCI-GDC/psqlgraph.git@7b5de7d56aa3159a9526940eb273579ddbf084ca#egg=psqlgraph',
+        'git+https://git@github.com/uc-cdis/cdis-python-utils.git@0.1.5#egg=cdispyutils',
+        'git+https://git@github.com/uc-cdis/userdatamodel.git@f25f5dfb356e80c45153ffd3e91b399be672a81c#egg=userdatamodel',
+        'git+https://git@github.com/uc-cdis/cdis_oauth2client.git@0.1.1#egg=cdis_oauth2client',
+        'git+https://git@github.com/NCI-GDC/cdisutils.git@a79409a0ce5071a81c6997d4ed1549c3544fbdcd#egg=cdisutils',
+        'git+https://git@github.com/uc-cdis/datadictionary.git@0.1.1#egg=gdcdictionary',
+        'git+https://git@github.com/NCI-GDC/gdcdatamodel.git@755c6d7c380b69dc36dced55700bc9e24a084db1#egg=gdcdatamodel',
     ],
 )
