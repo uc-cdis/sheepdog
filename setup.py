f37f039a35b6671f14c1cf967eb89267ead090ce
Jimmy <OpenProfOak@gmail.com>
2018-01-11 17:19:50 -0600

    feat(api-373): remove comments from setup.py

--- setup.py
+++ setup.py
@@ -51,8 +51,6 @@ setup(
         'userdatamodel',
     ],
     dependency_links=[
-        # 'git+https://git@github.com/uc-cdis/datadictionary.git@0.1.1#egg=gdcdictionary',
-        # 'git+https://git@github.com/NCI-GDC/gdcdatamodel.git@755c6d7c380b69dc36dced55700bc9e24a084db1#egg=gdcdatamodel',
         'git+https://git@github.com/uc-cdis/userdatamodel.git@cb7143c709a1173c84de4577d3e866318a2cc834#egg=userdatamodel', # OK
         'git+https://git@github.com/uc-cdis/cdis_oauth2client.git@0.1.2#egg=cdis_oauth2client', # OK
         'git+https://git@github.com/NCI-GDC/psqlgraph.git@1.2.0#egg=psqlgraph', # OK
