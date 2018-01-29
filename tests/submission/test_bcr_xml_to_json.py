b082f48c10dc184ba0a1e8ef955eb4aa786c6b76
Justin Barnowski <jbarno@uchicago.edu>
2018-01-29 15:19:35 -0600

    fix(test): munge property not gdc_to_types

--- tests/submission/test_bcr_xml_to_json.py
+++ tests/submission/test_bcr_xml_to_json.py
@@ -1,40 +1,38 @@
-from sheepdog.utils.transforms.bcr_xml_to_json import BcrXmlToJsonParser
+from sheepdog.utils.transforms.bcr_xml_to_json import munge_property 
 
 
 def test_gdc_type_mappings():
 
-       parser = BcrXmlToJsonParser("SAMPLE")
-
        # parse float
-       float_val = parser.map_to_gdc_types("float", "38.3")
+       float_val = munge_property("float", "38.3")
        self.assertTrue(type(float_val), float)
-       self.assertRaises(ValueError, parser.map_to_gdc_types, "float", "38k")
+       self.assertRaises(ValueError, munge_property, "float", "38k")
 
        # parse int
-       int_val = parser.map_to_gdc_types("int", "38")
+       int_val = munge_property("int", "38")
        self.assertTrue(type(int_val), int)
-       self.assertRaises(ValueError, parser.map_to_gdc_types, "int", "4.9")
+       self.assertRaises(ValueError, munge_property, "int", "4.9")
 
        # parse long
-       long_val = parser.map_to_gdc_types("long", "3111118")
+       long_val = munge_property("long", "3111118")
        self.assertTrue(type(long_val), long)
-       self.assertRaises(ValueError, parser.map_to_gdc_types, "long", "4.9")
+       self.assertRaises(ValueError, munge_property, "long", "4.9")
 
        # parse str
-       str_val = parser.map_to_gdc_types("str", "Dummy Text")
+       str_val = munge_property("str", "Dummy Text")
        self.assertTrue(type(str_val), str)
        self.assertEqual(str_val, "Dummy Text")
 
        # parse str lower
-       lower_val = parser.map_to_gdc_types("str.lower", "Dummy Text")
+       lower_val = munge_property("str.lower", "Dummy Text")
        self.assertTrue(type(lower_val), str)
        self.assertEqual(lower_val, "dummy text")
 
        # parse bool
-       self.assertTrue(parser.map_to_gdc_types("bool", "yes"))
-       self.assertTrue(parser.map_to_gdc_types("bool", "true"))
-       self.assertFalse(parser.map_to_gdc_types("bool", "no"))
-       self.assertFalse(parser.map_to_gdc_types("bool", "false"))
+       self.assertTrue(munge_property("bool", "yes"))
+       self.assertTrue(munge_property("bool", "true"))
+       self.assertFalse(munge_property("bool", "no"))
+       self.assertFalse(munge_property("bool", "false"))
 
-       self.assertRaises(ValueError, parser.map_to_gdc_types, "bool", "NAY")
+       self.assertRaises(ValueError, munge_property, "bool", "NAY")
 
