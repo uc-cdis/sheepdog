1d5de91d3ef0c164e9cb2ea0851cb2d027106f20
Justin Barnowski <jbarno@uchicago.edu>
2018-01-29 16:58:39 -0600

    fix(test): Fix the order of args in test_bcr_xml

--- tests/submission/test_bcr_xml_to_json.py
+++ tests/submission/test_bcr_xml_to_json.py
@@ -4,35 +4,35 @@ from sheepdog.utils.transforms.bcr_xml_to_json import munge_property
 def test_gdc_type_mappings():
 
        # parse float
-       float_val = munge_property("float", "38.3")
+       float_val = munge_property("38.3", "float")
        self.assertTrue(type(float_val), float)
-       self.assertRaises(ValueError, munge_property, "float", "38k")
+       self.assertRaises(ValueError, munge_property, "38k", "float")
 
        # parse int
-       int_val = munge_property("int", "38")
+       int_val = munge_property("38", "int")
        self.assertTrue(type(int_val), int)
-       self.assertRaises(ValueError, munge_property, "int", "4.9")
+       self.assertRaises(ValueError, munge_property, "4.9", "int")
 
        # parse long
-       long_val = munge_property("long", "3111118")
+       long_val = munge_property("3111118", "long")
        self.assertTrue(type(long_val), long)
-       self.assertRaises(ValueError, munge_property, "long", "4.9")
+       self.assertRaises(ValueError, munge_property, "4.9", "long")
 
        # parse str
-       str_val = munge_property("str", "Dummy Text")
+       str_val = munge_property("Dummy Text", "str")
        self.assertTrue(type(str_val), str)
        self.assertEqual(str_val, "Dummy Text")
 
        # parse str lower
-       lower_val = munge_property("str.lower", "Dummy Text")
+       lower_val = munge_property("Dummy Text", "str.lower")
        self.assertTrue(type(lower_val), str)
        self.assertEqual(lower_val, "dummy text")
 
        # parse bool
-       self.assertTrue(munge_property("bool", "yes"))
-       self.assertTrue(munge_property("bool", "true"))
-       self.assertFalse(munge_property("bool", "no"))
-       self.assertFalse(munge_property("bool", "false"))
+       self.assertTrue(munge_property("yes", "bool"))
+       self.assertTrue(munge_property("true", "bool"))
+       self.assertFalse(munge_property("no", "bool"))
+       self.assertFalse(munge_property("false", "bool"))
 
-       self.assertRaises(ValueError, munge_property, "bool", "NAY")
+       self.assertRaises(ValueError, munge_property, "NAY", "bool")
 
