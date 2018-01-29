ad34a6bfa23320ec34c75eef75df67d9167a874d
Justin Barnowski <jbarno@uchicago.edu>
2018-01-29 17:57:27 -0600

    fix(tests): Codacy errors

--- tests/submission/test_bcr_xml_to_json.py
+++ tests/submission/test_bcr_xml_to_json.py
@@ -6,30 +6,30 @@ def test_gdc_type_mappings():
 
        # parse float
        float_val = munge_property("38.3", "float")
-       assert type(float_val) == float
+       assert isinstance(float_val, float)
        with pytest.raises(ValueError):
            munge_property("38k", "float")
 
        # parse int
        int_val = munge_property("38", "int")
-       assert type(int_val) == int
+       assert isinstance(int_val, int)
        with pytest.raises(ValueError):
            munge_property("4.9", "int")
 
        # parse long
        long_val = munge_property("3111118", "long")
-       assert type(long_val) == long
+       assert isinstance(long_val, long)
        with pytest.raises(ValueError):
            munge_property("4.9", "long")
 
        # parse str
        str_val = munge_property("Dummy Text", "str")
-       assert type(str_val) == str
+       assert isinstance(str_val, str)
        assert str_val == "Dummy Text"
 
        # parse str lower
        lower_val = munge_property("Dummy Text", "str.lower")
-       assert type(lower_val) == str
+       assert isinstance(lower_val, str)
        assert lower_val == "dummy text"
 
        # parse bool
