b49499bd68df5c97d25d38fd91c72dc913124c2e
Justin Barnowski <jbarno@uchicago.edu>
2018-01-29 16:07:57 -0600

    fix(deps): Pin Werkzeug to 1.12 Temporary fix

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         'setuptools==30.1.0',
         'simplejson==3.8.1',
         'python-keystoneclient==1.8.1',
+        'Werkzeug==0.12.1',
         'authutils',
         'cdispyutils',
         'cdislogging',
