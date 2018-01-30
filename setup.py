74d82f7146580094235414f8f249253b3ab9572b
Jimmy <OpenProfOak@gmail.com>
2018-01-30 16:20:57 -0600

    feat(api-373): move around deps, re-add find_packages

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'boto',
-        'psycopg2>=2.7.3',
+        'psycopg2',
         'cryptography',
         'Envelopes',
         'Flask-Cors',
