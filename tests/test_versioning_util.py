2dd4bd2baf6e1ec84333beb14eeb0dda88958e43
Rowland Ogwara <rogwara@uchicago.edu>
2018-03-23 12:13:50 -0500

    fixed class VersionHelper name issue

--- tests/test_versioning_util.py
+++ tests/test_versioning_util.py
@@ -2,14 +2,17 @@ import unittest
 
 import pytest as pytest
 
-from sheepdog.utils.versioning import VersionHelper
+from sheepdog.utils.versioning import IndexVersionHelper
 
 
 class VersionHelperTest(unittest.TestCase):
 
     @pytest.fixture(autouse=True)
     def init(self, index_client):
-        self.vh = VersionHelper(index_client)
+        self.vh = IndexVersionHelper(index_client)
+
+    def test_release_node(self):
+        self.fail("Not Implemented")
 
     def test_add_node_revision(self):
         self.fail("Not Implemented")
