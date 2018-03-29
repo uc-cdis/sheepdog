103b509c544dfbefb7592fccd5ef9372d1e5a33a
Rowland Ogwara <rogwara@uchicago.edu>
2018-03-29 12:06:06 -0500

    added method for releasing project nodes

--- sheepdog/utils/versioning.py
+++ sheepdog/utils/versioning.py
@@ -1,16 +1,20 @@
 """
 Utility functions for handling common tasks in versioning
 """
-from indexclient.client import Document
+from cdislogging import get_logger
+from indexclient.client import Document, IndexClient
 
 
 class IndexVersionHelper:
 
-    def __init__(self, index_client):
+    def __init__(self, index_client, psql_driver):
         """
         Args:
             index_client (indexclient.client.IndexClient):
+            psql_driver (psqlgraph.PsqlGraphDriver)
         """
+        self.logger = get_logger("versioning.IndexVersionHelper")
+        self.g = psql_driver
         self.index_client = index_client
 
     def add_node_version(self, family_member_gdc_id, hashes, size, file_name=None, urls=None, metadata=None):
@@ -61,11 +65,12 @@ class IndexVersionHelper:
             gdc_node_id (str): The GDC node_id of the node to be released
 
         Return:
-            bool: True if release happened, else False
+            tuple[bool, str]: True if release happened, else False, the latest gdc_release_number
         """
 
         versions = self.index_client.list_versions(gdc_node_id)  # type: list[indexclient.client.Document}
         latest_version_number = 0
+        latest_version = None
         latest_unversioned = None
 
         for version in versions:
@@ -74,13 +79,43 @@ class IndexVersionHelper:
                 latest_unversioned = version
             elif int(version.version) > latest_version_number:
                 latest_version_number = int(version.version)
+                latest_version = version
 
         if latest_unversioned is not None:
             latest_unversioned.version = str(latest_version_number + 1)
-            latest_unversioned.metadata["gdc_release_number"] = gdc_release_number
+            latest_unversioned.metadata["release_number"] = gdc_release_number
             latest_unversioned.patch()
-            return True
-        return False
+            self.logger.info("Release updated for: {}".format(latest_unversioned.did))
+            return True, gdc_release_number
+        return False, latest_version.metadata.get("release_number")
+
+    def get_unversioned_nodes(self):
+
+        pass
+
+    def do_project_release(self, release_number, project_id):
+        """
+        For the given project,
+            * look up all nodes that are in the submitted state
+            * For each of these nodes perform a release
+            * Update the state of the node to released
+        Args:
+            release_number(str): The currently active release number
+            project_id(str): project id of nodes being released
+        Returns:
+             int: count of nodes successfully released
+        """
+        # get unreleased project nodes
+
+        with self.g.session_scope() as _:
+
+            un_released_nodes = self.g.nodes().props(state="submitted", project_id=project_id).all()
+            for node in un_released_nodes:
+                released, latest_release_number = self.release_node(release_number, node.id)
+                if latest_release_number == release_number:
+                    # entry has already been released
+                    node.props[""]
+                    pass
 
     def do_release(self, gdc_release_number):
         """
@@ -91,10 +126,18 @@ class IndexVersionHelper:
         Returns:
              int: number of nodes updated
         """
-        print("Performing GDC Release: {}".format(gdc_release_number))
-        unversioned = self.index_client.list_with_params(params={"version": None})
+        self.logger.info("Performing GDC Release: {}".format(gdc_release_number))
+        unversioned = self.index_client.list_with_params(params={"version": 'None'})
         released_count = 0
         for version in unversioned:
             if self.release_node(gdc_release_number, version.did):
                 released_count += 1
         return released_count
+
+
+# Just Testing
+if __name__ == '__main__':
+    client = IndexClient('http://172.21.13.60','v0', ('dev_indexd', '7"s^=eLIqKbw%qA8'))
+    vh = IndexVersionHelper(client)
+
+    print(vh.do_release("12"))
