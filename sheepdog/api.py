5f4604f240122be154ff5e35f8c9aac2f4f92cf4
Rudyard Richter <rudyardrichter@uchicago.edu>
2019-02-07 10:38:17 -0600

    fix(snapshots): call migrate

--- sheepdog/api.py
+++ sheepdog/api.py
@@ -86,6 +86,7 @@ def db_init(app):
 
 
 def migrate_database(app):
+    postgres_admin.migrate_transaction_snapshots(app.db, read_role)
     if postgres_admin.check_version(app.db):
         return
     try:
