4227fcb0a8b8e441bc27464d94b1e6d2f467303a
Fantix King <fantix.king@gmail.com>
2019-04-16 17:48:47 -0500

    fix(export): disable timeout (#275)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import flask
+
 from fence.jwt.token import generate_signed_access_token
 import pytest
 
@@ -92,3 +94,20 @@ def member(create_user_header):
     project_ids = ["phs000218", "phs000235", "phs000178"]
     project_access = {project: ["_member"] for project in project_ids}
     return create_user_header(MEMBER_USERNAME, project_access)
+
+
+@pytest.yield_fixture
+def client(app):
+    """
+    Overriding the `client` fixture from pytest_flask to fix this bug:
+    https://github.com/pytest-dev/pytest-flask/issues/42
+    """
+    with app.test_client() as client:
+        yield client
+
+    while True:
+        top = flask._request_ctx_stack.top
+        if top is not None and top.preserved:
+            top.pop()
+        else:
+            break
