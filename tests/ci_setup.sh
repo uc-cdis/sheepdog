b3610a29d46eb4d3c654c46bd2bfe076f0ecd1bc
Alex VanTol <avantol@uchicago.edu>
2023-09-12 14:17:46 -0500

    fix(gh): ensure python commands run in venv

--- tests/ci_setup.sh
+++ tests/ci_setup.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-pip freeze
-python bin/setup_test_database.py
+poetry run python bin/setup_test_database.py
 mkdir -p tests/integration/resources/keys; cd tests/integration/resources/keys; openssl genrsa -out test_private_key.pem 2048; openssl rsa -in test_private_key.pem -pubout -out test_public_key.pem; cd -
