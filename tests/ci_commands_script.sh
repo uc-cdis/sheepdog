8010f36f8420448e231c2eae16290e327b8e0680
Alex VanTol <avantol@uchicago.edu>
2023-09-15 10:36:16 -0500

    chore(tests): simplify test run script and test failure

--- tests/ci_commands_script.sh
+++ tests/ci_commands_script.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
+
+# since this whole thing is run as a bash {{this script}}, only the last pytest
+# command controls the exit code. We actually want to exit early if something fails
+set -e
+
 # datadict and datadictwithobjid tests must run separately to allow
 # loading different datamodels
 poetry run pytest -vv --cov=sheepdog --cov-report xml tests/integration/datadict
-
-# since this whole thing is run as a bash {{this script}}, only the last pytest
-# command controls the exit code. We actually want to exit early if something fails,
-# so check that here and exit as necessary
-RESULT=$?
-if [ $RESULT -ne 0 ]; then
-  exit 1
-fi
-
 poetry run pytest -vv --cov=sheepdog --cov-report xml --cov-append tests/integration/datadictwithobjid
-
-RESULT=$?
-if [ $RESULT -ne 0 ]; then
-  exit 1
-fi
-
 poetry run pytest -vv --cov=sheepdog --cov-report xml --cov-append tests/unit
