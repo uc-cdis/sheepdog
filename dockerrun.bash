dca954424833ce93a7c5353a7ae392cbf75cf6dd
George Thomas <george42@uchicago.edu>
2024-09-04 09:46:23 -0700

    Flip order of gunicorn and nginx in docker bash

--- dockerrun.bash
+++ dockerrun.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# nginx
+gunicorn -c "/sheepdog/deployment/wsgi/gunicorn.conf.py" &
+echo "gunicorn submitted"
+sleep 60
+echo "starting nginx"
 nginx
-gunicorn -c "/sheepdog/deployment/wsgi/gunicorn.conf.py"
