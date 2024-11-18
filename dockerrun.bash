7308e9ecb601e67f8105281e5077dfd52a4bb34a
EliseCastle23 <109446148+EliseCastle23@users.noreply.github.com>
2024-11-18 15:33:54 -0700

    Update dockerrun.bash

--- dockerrun.bash
+++ dockerrun.bash
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-poetry run gunicorn -c "/sheepdog/deployment/wsgi/gunicorn.conf.py" &
-# sleep to prevent nginx warning logs so nginx doesn't start before the app
-sleep 30
-nginx -g 'daemon off;'
+nginx 
+poetry run gunicorn -c "/sheepdog/deployment/wsgi/gunicorn.conf.py"
