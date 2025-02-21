f3e556e0f4a61879540647e3dd94ffdb574eec69
Sai Shanmukha <nss10@outlook.com>
2025-02-21 11:58:06 -0600

    Testing to see if sheepdog gunicorn conf can help reduce sheepdog crashing

--- deployment/wsgi/gunicorn.conf.py
+++ deployment/wsgi/gunicorn.conf.py
@@ -1,9 +1,10 @@
+import multiprocessing
+
 wsgi_app = "bin.settings:application"
 bind = "0.0.0.0:8000"
-workers = 1
+workers = multiprocessing.cpu_count() * 2 + 1
 preload_app = True
 user = "gen3"
 group = "gen3"
 timeout = 300
-keepalive = 2
-keepalive_timeout = 5
+keepalive = 10
