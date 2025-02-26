3c9b8374409875b08aa8ac557b7abebdaa60ae74
Sai Shanmukha <nss10@outlook.com>
2025-02-26 14:48:20 -0600

    Add `worker_type`

--- deployment/wsgi/gunicorn.conf.py
+++ deployment/wsgi/gunicorn.conf.py
@@ -3,6 +3,7 @@ import multiprocessing
 wsgi_app = "bin.settings:application"
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
+worker_type = "gevent"
 preload_app = True
 user = "gen3"
 group = "gen3"
