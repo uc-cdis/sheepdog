e8a543f0f38ec3bd7ee6126a147a3cf57e0ac187
Piotr Senkow <piotrsenkow@gmail.com>
2025-02-26 15:41:42 -0600

    using psuedo threads

--- deployment/wsgi/gunicorn.conf.py
+++ deployment/wsgi/gunicorn.conf.py
@@ -3,7 +3,8 @@ import multiprocessing
 wsgi_app = "bin.settings:application"
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
-worker_type = "gevent"
+worker_class = "gevent"
+worker_connections = 1000
 preload_app = True
 user = "gen3"
 group = "gen3"
