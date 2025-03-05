c02d85602c4a5d665de0bdd7a4bbf8da61cac491
Sai Shanmukha <nss10@outlook.com>
2025-03-05 14:45:32 -0600

    Adding gunicorn logs

--- deployment/wsgi/gunicorn.conf.py
+++ deployment/wsgi/gunicorn.conf.py
@@ -9,5 +9,11 @@ preload_app = False
 user = "gen3"
 group = "gen3"
 timeout = 300
+graceful_timeout = 45
 keepalive = 10
 pidfile = "/sheepdog/gunicorn.pid"
+
+
+accesslog = "-"  # Logs to stdout (disable if needed)
+errorlog = "-"  # Logs errors to stdout
+loglevel = "info"  # Change to "debug" for more verbosity
