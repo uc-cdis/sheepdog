d432df8f920bb6283350dd55e1827e8bc16437ba
Sai Shanmukha <nss10@outlook.com>
2025-03-04 19:53:03 -0600

    Changing number of workers back to 1 to see if there is a performance crash

--- deployment/wsgi/gunicorn.conf.py
+++ deployment/wsgi/gunicorn.conf.py
@@ -2,7 +2,7 @@ import multiprocessing
 
 wsgi_app = "bin.settings:application"
 bind = "0.0.0.0:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
 worker_class = "gevent"
 worker_connections = 1000
 preload_app = True
