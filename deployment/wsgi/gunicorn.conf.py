b52786a1230db09b2482cc9b631d1051eca5857d
Sai Shanmukha <nss10@outlook.com>
2025-03-07 09:20:26 -0600

    adding gevent back to gunicotn config

--- deployment/wsgi/gunicorn.conf.py
+++ deployment/wsgi/gunicorn.conf.py
@@ -3,6 +3,8 @@ import multiprocessing
 wsgi_app = "bin.settings:application"
 bind = "0.0.0.0:8000"
 workers = 4
+worker_class = "gevent"
+worker_connections = 25
 preload_app = False
 user = "gen3"
 group = "gen3"
