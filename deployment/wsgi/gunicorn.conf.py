b2940ab14bcf7a1ff86271a2e424a1b976e36046
Sai Shanmukha <nss10@outlook.com>
2025-03-12 18:33:11 -0500

    Change gunicorn config to remove unwanted keys

--- deployment/wsgi/gunicorn.conf.py
+++ deployment/wsgi/gunicorn.conf.py
@@ -1,10 +1,6 @@
-import multiprocessing
-
 wsgi_app = "bin.settings:application"
 bind = "0.0.0.0:8000"
 workers = 2
-worker_type = "gevent"
-worker_connections = 25
 preload_app = False
 user = "gen3"
 group = "gen3"
@@ -12,8 +8,3 @@ timeout = 300
 graceful_timeout = 45
 keepalive = 10
 pidfile = "/sheepdog/gunicorn.pid"
-
-
-accesslog = "-"  # Logs to stdout (disable if needed)
-errorlog = "-"  # Logs errors to stdout
-loglevel = "info"  # Change to "debug" for more verbosity
